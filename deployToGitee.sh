c8afacc87db1330c36614b6d6322631c23c29caf
刘文豪 <liuwenhao@fenxiangbuy.com>
2020-09-13 18:48:29 +0800

    添加gitee

--- deployToGitee.sh
+++ deployToGitee.sh
@@ -1,11 +1,11 @@
 #代码在Travis中编译完成后，顺便部署到gitee pages中
 #!/bin/bash
 cd docs/.vuepress/dist
-git config user.name "在那遥远的瞬间"
-git config user.email "z351622948@163.com"
+git config user.name "丶劉灏藴"
+git config user.email "lwh19900521@163.com"
 
 git init
 git add .
 git commit -m "发布pages"
-git remote add origin git@gitee.com:db117/db117.git
+git remote add origin git@gitee.com:liuhaoyun/lwh6a.git
 git push --force origin master:master
